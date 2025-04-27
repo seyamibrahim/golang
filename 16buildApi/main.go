cdf0e00f3ca8f4776b0cbdeb3d2c0bc27f8d6e62
seyam <seyamibrahim88@gmail.com>
2025-04-27 12:55:34 +0530

    Send a API json response

--- 16buildApi/main.go
+++ 16buildApi/main.go
@@ -1,11 +1,20 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 // Model for course -file
 type Course struct {
 	CourseId    string  `json:"courseid"`
 	CourseName  string  `json:"coursename"`
 	CoursePrice int     `json:"price"`
-	Author      *Author `json:"author"`
+	// Author      *Author `json:"author"`
 }
 type Author struct {
 	Fullname string `json:"fullname"`
@@ -14,14 +23,35 @@ type Author struct {
 
 // fake DB
 
-var courses []Course
+
+var courses = []Course{{"adfslk", "React",199},{"flaskdjf", "Go", 299},}
+
+
 
 // middleware , helper -file
 
-func (c *Course) IsEmpty() bool{
+func (c *Course) IsEmpty() bool {
 	return c.CourseId == "" && c.CourseName == ""
 }
 
 func main() {
+	
+	r := mux.NewRouter()
+	// routes
+	r.HandleFunc("/", ServeHome).Methods("GET")
+	
+	r.HandleFunc("/getallcourses", GetAllCourses).Methods("GET")
+	
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
 
+func ServeHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to Api Learning</h1>"))
+}
+
+func GetAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
